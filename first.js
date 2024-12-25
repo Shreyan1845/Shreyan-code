d89cb7a73dff155c5b08c2a7ca2e8053b43c752b
Shreyan1845 <sunil.sushil435.sk@gmail.com>
2024-12-25 18:47:45 +0530

    Add files via upload
    Hey.........

---
+++ first.js
@@ -0,0 +1,21 @@
+console.log("Welcome to JavaScript")
+console.log("Shreyan")
+console.log("class 10th")
+console.log("Varibles are containers for data")
+fullname="Tony stark";
+age="59";
+price="144.00 million rs"
+Radius="15"
+x = null;
+y = undefined;
+isfollow = true;
+Language="JavaScript"
+type="Coding Language"
+console.log(age);
+console.log(price);
+console.log(Radius)
+console.log(x)
+console.log(y)
+console.log(isfollow)
+console.log(Language)
+console.log(type)
